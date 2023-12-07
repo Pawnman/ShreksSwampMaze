aa25ac9fda8f688bd9079bc31bb6806297612618
Pawnman <maks-peshkov@mail.ru>
2023-12-08 01:28:11 +0300

    Level_2
    Added Level_2 but it needs some fixes (tables and gameover scene).

--- ShrekMaze3D/Assets/ShrekMaze/Scripts/TestH.cs
+++ ShrekMaze3D/Assets/ShrekMaze/Scripts/TestH.cs
@@ -18,7 +18,7 @@ public class TestH : MonoBehaviour
 
             if (hpForHero == 0)
             {
-                SceneManager.LoadScene(3);
+                SceneManager.LoadScene(4);
             }
         }
  
