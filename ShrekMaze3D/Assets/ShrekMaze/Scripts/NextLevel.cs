4218bd1eb2ef986caf236a89cd7d488cea16d9c8
Pawnman <maks-peshkov@mail.ru>
2023-12-09 16:24:44 +0300

    Level_2 Terrain
    Created Terrain for level_2. Need fix table points with finish block.

--- ShrekMaze3D/Assets/ShrekMaze/Scripts/NextLevel.cs
+++ ShrekMaze3D/Assets/ShrekMaze/Scripts/NextLevel.cs
@@ -7,12 +7,13 @@ public class NextLevel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //PlayerInventory.TP == 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(PlayerInventory.TP);
         if (PlayerInventory.TP == 6)
         {
             gameObject.SetActive(false);
